29c223695b4e6922ead59131e7d29ab2d62aa2fc
DiegoU <DiegoMado@users.noreply.github.com>
2016-10-20 18:50:51 +0200

    Son solo esquemas vacíos

--- SecurVision/src/com/userInterface/ControladorLogginAdmin.java
+++ SecurVision/src/com/userInterface/ControladorLogginAdmin.java
@@ -1,5 +1,38 @@
 package com.SecurVision.userInterface;
 
-public class ControladorLogginAdmin {
+import java.net.URL;
+import java.time.LocalDateTime;
 
-}
+import java.time.LocalTime;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import com.SecurVision.logic.*;
+
+public class ControladorLogginAdmin extends ControladorCasoDeUso{
+	
+	
+	@Override
+	 public void initialize(URL location, ResourceBundle resources) {
+	 stage = new Stage(StageStyle.DECORATED);
+	 stage.setTitle("Loggin");
+	
+	 //Invocamos el servicio encargado de Loggin Administrador
+		 
+	try {
+		
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+
+	 stage.close();
+	 };
+	 }
--- SecurVision/src/com/userInterface/ControladorVerPerfilUsuario.java
+++ SecurVision/src/com/userInterface/ControladorVerPerfilUsuario.java
@@ -1,5 +1,38 @@
 package com.SecurVision.userInterface;
 
-public class ControladorVerPerfilUsuario {
+import java.net.URL;
+import java.time.LocalDateTime;
 
-}
+import java.time.LocalTime;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import com.SecurVision.logic.*;
+
+public class ControladorVerPerfilUsuario extends ControladorCasoDeUso{
+	
+	
+	@Override
+	 public void initialize(URL location, ResourceBundle resources) {
+	 stage = new Stage(StageStyle.DECORATED);
+	 stage.setTitle("Ver Perfil de Usuario");
+	
+	
+	 //Invocamos el servicio encargado de Ver Perfil de Usuario
+		 
+	try {
+		
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	 stage.close();
+	 };
+	 }
